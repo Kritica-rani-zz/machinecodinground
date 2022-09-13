31938861e3bf259a8e24f69a73d92bc334106103
Kritica-rani <kritica1997@gmail.com>
2022-09-13 14:10:06 +0530

    added addList

--- src/crud/Todolist.jsx
+++ src/crud/Todolist.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState } from "react";
 
 function Todolist() {
+  const [input, setInput] = useState('');
+  const [addList, setAddList] = useState([]);
+  const [editMode, setEditMode] = useState(false);
+  const handleAdd=(text)=>{
+  const item =({isEditable:false,id:Math.random(),body:text})
+  setAddList([item,...addList])
+  }
+
   return (
-    <div>Todolist</div>
-  )
-}
+  <div>
+    <div className="input-container">
+    <input type="text" placeholder="wishlist" value={input} onChange={(e)=>setInput(e.target.value)}/>
+    <button onClick={()=>handleAdd(input)} >Add</button>
+    </div>
+    {addList && addList.map((item)=>{
+      return(
+        <li>{item.body}</li>
+      )
+    })}
+  </div>
+)}
 
 export default Todolist;
